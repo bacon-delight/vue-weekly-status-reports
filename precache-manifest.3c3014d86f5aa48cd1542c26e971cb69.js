7e1bfa17393895a9ce5d42c0872d6fb34bd683c8
Dipanjan De <dipanjan131@gmail.com>
2019-07-13 19:07:07 +0530

    Initial dist subtree commit

---
+++ precache-manifest.3c3014d86f5aa48cd1542c26e971cb69.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = [
+  {
+    "revision": "092c230f11c061f8e8cb",
+    "url": "/weekly-status-reports/css/app.59a232a3.css"
+  },
+  {
+    "revision": "092c230f11c061f8e8cb",
+    "url": "/weekly-status-reports/js/app.3f67abd6.js"
+  },
+  {
+    "revision": "62f39dc7c53f346b6201",
+    "url": "/weekly-status-reports/css/chunk-1019ee05.966d8dae.css"
+  },
+  {
+    "revision": "62f39dc7c53f346b6201",
+    "url": "/weekly-status-reports/js/chunk-1019ee05.3585ae60.js"
+  },
+  {
+    "revision": "9c6f8c2faa2320cffa64",
+    "url": "/weekly-status-reports/css/chunk-20a566f9.83f19d5c.css"
+  },
+  {
+    "revision": "9c6f8c2faa2320cffa64",
+    "url": "/weekly-status-reports/js/chunk-20a566f9.198cf70b.js"
+  },
+  {
+    "revision": "9ece1fa2f90c0111639c",
+    "url": "/weekly-status-reports/css/chunk-2ac7c5f4.ebefd57b.css"
+  },
+  {
+    "revision": "9ece1fa2f90c0111639c",
+    "url": "/weekly-status-reports/js/chunk-2ac7c5f4.22b9fe58.js"
+  },
+  {
+    "revision": "91d5f571fd31d81134df",
+    "url": "/weekly-status-reports/js/chunk-2d208527.e928a8c9.js"
+  },
+  {
+    "revision": "4648003bb6019f2a3aa9",
+    "url": "/weekly-status-reports/js/chunk-2e79d776.e514f43a.js"
+  },
+  {
+    "revision": "e6a611681e7946905aa8",
+    "url": "/weekly-status-reports/css/chunk-394e7a78.2612fe7d.css"
+  },
+  {
+    "revision": "e6a611681e7946905aa8",
+    "url": "/weekly-status-reports/js/chunk-394e7a78.55cd94a2.js"
+  },
+  {
+    "revision": "05cb71ea3cce94beaa0a",
+    "url": "/weekly-status-reports/css/chunk-568f7249.9f93b846.css"
+  },
+  {
+    "revision": "05cb71ea3cce94beaa0a",
+    "url": "/weekly-status-reports/js/chunk-568f7249.d5a058d0.js"
+  },
+  {
+    "revision": "361ef831831a96ae3dda",
+    "url": "/weekly-status-reports/css/chunk-65d7dc9e.1edc1141.css"
+  },
+  {
+    "revision": "361ef831831a96ae3dda",
+    "url": "/weekly-status-reports/js/chunk-65d7dc9e.8f0633fa.js"
+  },
+  {
+    "revision": "33bde1fa517bd7235488",
+    "url": "/weekly-status-reports/css/chunk-6640296b.966d8dae.css"
+  },
+  {
+    "revision": "33bde1fa517bd7235488",
+    "url": "/weekly-status-reports/js/chunk-6640296b.e5b79502.js"
+  },
+  {
+    "revision": "5d47aca6900f9de8446e",
+    "url": "/weekly-status-reports/css/chunk-vendors.79e4134e.css"
+  },
+  {
+    "revision": "5d47aca6900f9de8446e",
+    "url": "/weekly-status-reports/js/chunk-vendors.3274a53e.js"
+  },
+  {
+    "revision": "e2e7f1a547f5593f3d9c7441a6c879b2",
+    "url": "/weekly-status-reports/index.html"
+  },
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "/weekly-status-reports/robots.txt"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,29 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/weekly-status-reports/precache-manifest.3c3014d86f5aa48cd1542c26e971cb69.js"
+);
+
+workbox.core.setCacheNameDetails({prefix: "client-emp"});
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
